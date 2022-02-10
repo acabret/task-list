46560e3b172e6cbfd96d0615f02ceeb8ccd0bcfb
Ashisz <acabretpaz@gmail.com>
2022-02-09 22:48:42 -0300

    Se agergó la funcionalidad de actualizar la completitud de las tareas Se actualizo la obtención de las tareas desde el storage para reflejar el estado real de completitud

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const botonAgregar = document.querySelector('.btn-agregar')
 const inputTexto = document.querySelector("input")
 imprimirTareas(tareas)
 
+//{id:1, nombre:"asdasd", completado:false }
+
 botonAgregar.addEventListener("click", crearTarea)
 
 
@@ -21,17 +23,10 @@ function crearTarea(event){
 
 }
 
-
-
-
-
 function agregarUnaTarea(tarea){
 
-    const itemLista = document.createElement('li')
-    itemLista.innerText = tarea.nombre
-    listaTareas.appendChild(itemLista)
+    listaTareas.appendChild(crearElementoTarea(tarea))
     guardarTareasEnLocalStorage(tareas)
-
 
 }
 
@@ -41,13 +36,54 @@ function imprimirTareas(tareas){
     const fragmento = document.createDocumentFragment();
 
     for (let index = 0; index < tareas.length; index++) {
-        const itemLista = document.createElement('li')
-        itemLista.innerText = tareas[index].nombre
-        fragmento.appendChild(itemLista)
+        fragmento.appendChild(crearElementoTarea(tareas[index]))
     }
 
     listaTareas.appendChild(fragmento)
 
+}
+
+function crearElementoTarea(tarea){
+
+  const itemLista = document.createElement('li')
+
+  const spanTexto = document.createElement('span')
+  spanTexto.innerText = tarea.nombre
+  spanTexto.style.position = "relative";
+
+  tarea.completado ? spanTexto.classList.add('tarea-tachada') : spanTexto.classList.remove('tarea-tachada')
+
+  const botonCompletitud = document.createElement('button')
+  botonCompletitud.innerText = tarea.completado ? "Cancelar" : "Completar"
+  botonCompletitud.classList.add('btn', tarea.completado ? "btn-cancelar" : "btn-completar")
+
+  itemLista.classList.add('tarea')
+  itemLista.appendChild(spanTexto)
+  itemLista.appendChild(botonCompletitud)
+
+  botonCompletitud.addEventListener("click", function(){
+  tarea.completado = !tarea.completado
+  botonCompletitud.innerText = tarea.completado ? "Cancelar" : "Completar"
+    
+    if(tarea.completado){
+      spanTexto.classList.add('tarea-tachada')
+
+      botonCompletitud.classList.remove('btn-completar')
+      botonCompletitud.classList.add('btn-cancelar')
+
+    }else{
+      spanTexto.classList.remove('tarea-tachada')
+      
+      botonCompletitud.classList.add('btn-completar')
+      botonCompletitud.classList.remove('btn-cancelar')
+    }
+
+    guardarTareasEnLocalStorage(tareas)
+
+
+  })
+
+  return itemLista
 }
 
 
